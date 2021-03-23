e3666b9a687f0d1594104bfb5f8048fe34c1fed8
Bernardo Barata <b.barata@micro-biolytics.com>
2021-03-23 17:16:26 +0100

    Changes to declspec

--- TestLibrary/PumpInterface.cpp
+++ TestLibrary/PumpInterface.cpp
@@ -1,28 +1,28 @@
 #include "Pump.h"
 
-extern "C" __declspec(dllexport) PeristalticPump* CreateNewPump() {
+extern "C" PeristalticPump* CreateNewPump() {
 	return new PeristalticPump();
 }
 
-extern "C" __declspec(dllexport) void PumpAdd(PeristalticPump * Pump, int a, int b) {
+extern "C"  void PumpAdd(PeristalticPump * Pump, int a, int b) {
 	Pump->add(a, b);
 }
 
-extern "C" __declspec(dllexport) int GetResult(PeristalticPump * Pump) {
+extern "C"  int GetResult(PeristalticPump * Pump) {
 	return Pump->getResult();
 }
 
-extern "C" __declspec(dllexport) Status* GenerateStatus(PeristalticPump * Pump) {
+extern "C"  Status* GenerateStatus(PeristalticPump * Pump) {
 	Status statPtr = Pump->getStatus();
 	return &statPtr;
 }
 
-extern "C" __declspec(dllexport) PeristalticPumpWrapper * CreateNewPumpWrapper() {
+extern "C"  PeristalticPumpWrapper * CreateNewPumpWrapper() {
 	return new PeristalticPumpWrapper();
 }
 
 
-extern "C" __declspec(dllexport) Status * GenerateStatusWrapper(PeristalticPumpWrapper * Pump) {
+extern "C"  Status * GenerateStatusWrapper(PeristalticPumpWrapper * Pump) {
 	Status* statPtr = Pump->GetStatus();
 	return statPtr;
 }
--- TestLibrary/StatusInterface.cpp
+++ TestLibrary/StatusInterface.cpp
@@ -1,21 +1,21 @@
 #include "Status.h"
 
-extern "C" __declspec(dllexport) Status * CreateStatus() {
+extern "C"  Status * CreateStatus() {
 	return new Status();
 }
 
-extern "C" __declspec(dllexport) void SetSpeed(Status * status, int s) {
+extern "C"  void SetSpeed(Status * status, int s) {
 	status->setSpeed(s);
 }
 
-extern "C" __declspec(dllexport) int GetSpeed(Status * status) {
+extern "C"  int GetSpeed(Status * status) {
 	return status->getSpeed();
 }
 
-extern "C" __declspec(dllexport) void SetTemp(Status * status, int t) {
+extern "C"  void SetTemp(Status * status, int t) {
 	status->setTemp(t);
 }
 
-extern "C" __declspec(dllexport) int GetTemp(Status * status) {
+extern "C" int GetTemp(Status * status) {
 	return status->getTemp();
 }
